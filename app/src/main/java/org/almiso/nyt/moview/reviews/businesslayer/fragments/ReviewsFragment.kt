306f1e15a5ec3a04180463997a558f01eb197d56
Aleksandr Sosorev <alexandr.sosorev@gmail.com>
2018-10-01 22:37:09 +0300

    added rx and retrofit libs

--- app/src/main/java/org/almiso/nyt/moview/reviews/businesslayer/fragments/ReviewsFragment.kt
+++ app/src/main/java/org/almiso/nyt/moview/reviews/businesslayer/fragments/ReviewsFragment.kt
@@ -1,7 +1,6 @@
 package org.almiso.nyt.moview.reviews.businesslayer.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
